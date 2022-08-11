5bbd8faf4f5e3850c50feaa70aec716d5bf0260c
Sebastian Basconcelo <el_basco@hotmail.com>
2022-08-10 21:13:56 -0300

    agregar mock de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[]= [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 18 hs",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Investigar sobre Bootstrap",
+        day: "Agosto 5 a las 22 hs",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Hacer las compras para la cena",
+        day: "Agosto 6 a las 12 hs",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Leer un libro",
+        day: "Agosto 6 a las 18 hs",
+        reminder: true
+    }
+]
